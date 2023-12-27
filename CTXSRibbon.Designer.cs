263b065fadec20777aa1db20c3240496825e4547
oosio57 <urio@live.cn>
2023-12-27 11:09:07 +0800

    添加项目文件。

---
+++ CTXSRibbon.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace CTXS
+{
+    partial class CTXSRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public CTXSRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.MyMGroup = this.Factory.CreateRibbonGroup();
+            this.CTXSButton = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.MyMGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.MyMGroup);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // MyMGroup
+            // 
+            this.MyMGroup.Items.Add(this.CTXSButton);
+            this.MyMGroup.Label = "MyM";
+            this.MyMGroup.Name = "MyMGroup";
+            // 
+            // CTXSButton
+            // 
+            this.CTXSButton.Label = "修正轮胎系数";
+            this.CTXSButton.Name = "CTXSButton";
+            this.CTXSButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.CTXSButton_Click);
+            // 
+            // CTXSRibbon
+            // 
+            this.Name = "CTXSRibbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.CTXSRibbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.MyMGroup.ResumeLayout(false);
+            this.MyMGroup.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup MyMGroup;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton CTXSButton;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal CTXSRibbon CTXSRibbon
+        {
+            get { return this.GetRibbon<CTXSRibbon>(); }
+        }
+    }
+}
---
+++ CTXSRibbon.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Xml.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using System.IO;
+using System.Collections;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Drawing;
+
+namespace CTXS
+{
+    public partial class CTXSRibbon
+    {
+        private List<Excel.Range> SPTires = new List<Excel.Range>();
+        private List<Excel.Range> NSPTires = new List<Excel.Range>();
+
+        private Excel.Workbook Workbook = null;
+        private Excel.Worksheet Worksheet = null;
+
+        private long LastRow;
+        private long LastColumn;
+
+        private Excel.Range PartNuStartCell;
+        private Excel.Range PartNameStartCell;
+        private Excel.Range PSStartCell;
+
+        private const string TireKeyNum = "42751";
+        private const string SPTireKeyName = "D";
+        private void CTXSRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void CTXSButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            Workbook = Globals.ThisAddIn.Application.ActiveWorkbook;
+            Worksheet = Workbook.Worksheets[1];
+
+            LastRow = Worksheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell, Type.Missing).Row;
+            LastColumn = Worksheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell, Type.Missing).Column;
+
+            //LastRow = 9908;
+            //LastColumn = 212;
+
+            for(long R= 4;R<LastRow;R++)
+            {
+                if (Convert.ToString(Worksheet.Cells[R, 4].value).Substring(0, 5).Equals(TireKeyNum, StringComparison.OrdinalIgnoreCase))
+                {
+                    PartNuStartCell = Worksheet.Cells[R, 4];
+                    PartNameStartCell = Worksheet.Cells[R, 6];
+                    PSStartCell = Worksheet.Cells[R, 24];
+                    PartNuStartCell.BorderAround2(Excel.XlLineStyle.xlContinuous,
+                        Excel.XlBorderWeight.xlThick,
+                        Excel.XlColorIndex.xlColorIndexAutomatic,
+                        Excel.XlColorIndex.xlColorIndexAutomatic);
+
+                    PartNameStartCell.BorderAround2(Excel.XlLineStyle.xlContinuous,
+                        Excel.XlBorderWeight.xlThick,
+                        Excel.XlColorIndex.xlColorIndexAutomatic,
+                        Excel.XlColorIndex.xlColorIndexAutomatic);
+
+                    PSStartCell.BorderAround2(Excel.XlLineStyle.xlContinuous,
+                        Excel.XlBorderWeight.xlThick,
+                        Excel.XlColorIndex.xlColorIndexAutomatic,
+                        Excel.XlColorIndex.xlColorIndexAutomatic);
+                    PartNuStartCell.Activate();
+                    break;
+                }
+            }
+
+            //PartNuStartCell = Worksheet.Cells[4, 4];
+            //PartNameStartCell = Worksheet.Cells[4, 6];
+            //PSStartCell = Worksheet.Cells[4, 24];
+
+            MessageBox.Show($"待处理的最后一行是 {LastRow} 、最后一列是 {LastColumn}");
+
+            long Col = 1;
+            long Row = 1;
+            long xsCount = 0;
+
+            for (Col = PSStartCell.Column; Col < LastColumn + 1; Col++)
+            {
+                for (Row = PSStartCell.Row; Row < LastRow + 1; Row++)
+                {
+                    if (Convert.ToDouble(Worksheet.Cells[Row, Col].Value) == 0.0)
+                    {
+                        continue;
+                    }
+                    if (!Convert.ToString(Worksheet.Cells[Row, 4].value).Substring(0,5).Equals(TireKeyNum, StringComparison.OrdinalIgnoreCase))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        if (Convert.ToString(Worksheet.Cells[Row, 6].value).ToLower().Contains(SPTireKeyName.ToLower()))
+                        {
+                            Worksheet.Cells[Row, 6].Activate();
+                            Worksheet.Cells[Row, 6].Interior.Color = Color.Yellow;
+                            SPTires.Add(Worksheet.Cells[Row, Col]);
+                        }
+                        else
+                        {
+                            Worksheet.Cells[Row, 6].Activate();
+                            Worksheet.Cells[Row, 6].Interior.Color = Color.Pink;
+                            NSPTires.Add(Worksheet.Cells[Row, Col]);
+                        }
+                    }
+                }
+                foreach (Excel.Range c in SPTires)
+                {
+                    c.Value = 1.0 / Convert.ToDouble(SPTires.Count);
+                    xsCount++;
+
+                }
+                double NSPXSSum = 0;
+                foreach (Excel.Range c in NSPTires)
+                {
+                    NSPXSSum = NSPXSSum + Convert.ToDouble(c.Value);
+                }
+                foreach (Excel.Range c in NSPTires)
+                {
+                    c.Activate();
+                    c.BorderAround2(Excel.XlLineStyle.xlContinuous,
+                        Excel.XlBorderWeight.xlThick,
+                        Excel.XlColorIndex.xlColorIndexAutomatic,
+                        Excel.XlColorIndex.xlColorIndexAutomatic);
+                    c.Value = Math.Round((Convert.ToDouble(c.Value) / NSPXSSum) * 4.0,2);
+                    xsCount++;
+                }
+                SPTires.Clear();
+                NSPTires.Clear();
+            }
+            MessageBox.Show($"共修正 {xsCount} 个轮胎总成的系数");
+        }
+    }
+}
